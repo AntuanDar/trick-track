613a5bde0bd91ecae0f67cd4b00f26019c0779a0
sergun87 <nosashev@yandex.ru>
2023-04-02 11:40:25 +0300

    Попытка прописать логику 2.04.2023г. Обработка события кнопки логин

--- trick-track-client/src/main/java/ru/spb/taranenkoant/trick/track/client/LoginPane.java
+++ trick-track-client/src/main/java/ru/spb/taranenkoant/trick/track/client/LoginPane.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -19,36 +20,53 @@ import ru.spb.taranenkoant.trick.track.client.fx.IFxmlPane;
 
 public class LoginPane implements IFxmlPane {
 
-    //убрать,это что-то левое
-    @FXML private ResourceBundle resources;
-    //убрать,это что-то левое
-    @FXML private URL location;
-    @FXML private Button SignCancelButton;
-    @FXML private Button SignCancelLogin;
-    @FXML private AnchorPane rootPane;
+
+    private URL location;
+    @FXML
+    private Button SignCancelButton;
+    @FXML
+    private Button SignCancelLogin;
+    @FXML
+    private AnchorPane rootPane;
     //логин должен быть простым текстфилдом, а не парольным
-    @FXML private PasswordField signUpLogin;
-    @FXML private PasswordField signUpPass;
-
-
-    // cancel(ActionEvent event) и login(ActionEvent event) лучше убери, какая-то фигня, видимо билдер нагенерил
     @FXML
-    void cancel(ActionEvent event) {
-        SignCancelLogin.setOnAction(event1 ->  {
-            System.out.println("Вы нажали cancel");
-        });
-
-    }
-
-
+    private PasswordField signUpLogin;
     @FXML
-    void login(ActionEvent event) {
+    private PasswordField signUpPass;
 
-    }
+
+
 
     /**
      * Обработка событий кнопка Логин для перехода на файл MainPane
      */
+
+    /**
+     * Пытался прописать логику что если не успешно то ноль
+     * Иначе успешно и сообщение об успешности авторизации и загрузить панель.
+     */
+
+    @FXML
+    void login(ActionEvent event) {
+        Login.setOnAction(new ActionEvent());{
+            public void showLogin()  {
+                LoginStage loginStage = new LoginStage();
+                loginStage.setOnOk(() -> {
+                    if (loginStage.getLoginStatus() != LoginStatus.SUCCESS)
+                        System.exit(0);
+                    else(LoginStage.getLoginStatus() = LoginStatus.SUCCESS)
+                            System.out.println("Вы успешно авторизированы");
+                    loader.setLocation(getClass().getResource("resources/ru.spb.taranenkoant.trick.track.client.MainPane/MainPane.fxml"));
+
+
+                    showModuleContainer();
+                });
+                loginStage.showAndWait();
+            }
+            }
+        }
+
+
     @FXML
     void initialize() {
         signUpLogin.setOnAction(event -> {
@@ -62,8 +80,7 @@ public class LoginPane implements IFxmlPane {
             loader.setLocation(getClass().getResource("resources/ru.spb.taranenkoant.trick.track.client.MainPane/MainPane.fxml"));
 
             try {
-                //у тебя loader  написан с большой буквы, поэтому здесь идея подсвечивает как ошибку
-                //loader сделал с маленькой буквой
+
                 loader.load();
             }  catch (IOException e) {
                 e.printStackTrace();
